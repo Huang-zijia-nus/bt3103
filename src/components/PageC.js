da34981d0227dd57416364980b0914cf0dd42a9e
He Yingxu <yingxu.he1998@gmail.com>
2018-03-28 18:15:21 +0800

    add graph

--- src/components/PageC.js
+++ src/components/PageC.js
@@ -3,6 +3,11 @@ import { Link } from 'react-router-dom'
 import { connect } from 'react-redux'
 
 
+import * as V from 'victory';
+import { VictoryChart, VictoryBar, VictoryTheme, VictoryAxis, VictoryStack } from 'victory';
+import { compose } from 'redux'
+
+
 class PageC extends React.Component {
   value = this.props.value;
   render() {
@@ -10,6 +15,28 @@ class PageC extends React.Component {
       <div>
         <h2>Instructor Page</h2>
         <text>yes</text>
+
+        <VictoryChart
+          domainPadding={20}
+          theme={VictoryTheme.material}
+        >
+
+          <VictoryAxis
+            tickValues={[1, 2, 3, 4]}
+            tickFormat={["School 1", "school 2", "school 3", "school 4"]}
+          />
+
+          <VictoryAxis
+            dependentAxis
+            tickFormat={(x) => (`${x} ppl`)}
+          />
+
+          <VictoryBar
+            data={this.props.value.schools.Juniors}
+            //x=""
+            y="participants"
+          />
+        </VictoryChart>
       </div>
     )
   }
